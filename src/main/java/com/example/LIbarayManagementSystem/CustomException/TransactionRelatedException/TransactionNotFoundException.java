11107e2e126f065e4f79501ec4b1cd12e8fc8063
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-09-09 00:48:33 +0530

    Saved

---
+++ src/main/java/com/example/LIbarayManagementSystem/CustomException/TransactionRelatedException/TransactionNotFoundException.java
@@ -0,0 +1,2 @@
+package com.example.LIbarayManagementSystem.CustomException.TransactionRelatedException;public class TransactionNotFoundException {
+}
---
+++ src/main/java/com/example/LIbarayManagementSystem/ResponceDto/GetMaxFineBookDto.java
@@ -0,0 +1,2 @@
+package com.example.LIbarayManagementSystem.ResponceDto;public class GetMaxFineBookDto {
+}
